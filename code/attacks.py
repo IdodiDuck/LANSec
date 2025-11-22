9a000f43e887dae77fe89d46973582f5b34ee0e8
IdodiDuck <ido.redid@gmail.com>
2025-11-22 17:12:04 +0200

    feat: integrated logger in detection engine

--- code/attacks.py
+++ code/attacks.py
@@ -1,34 +1,52 @@
 import time
 from colorama import Fore, init as color_init
 color_init(autoreset=True)
-print("Attacks Module Loaded")
 
-from detect import randomized_mac, syn_scan, dir_traversal, arp_spoof, syn_flood
+from detect import (
+    randomized_mac,
+    syn_scan,
+    dir_traversal,
+    arp_spoof,
+    syn_flood,
+    ssh_bruteforce
+)
 
-
-# TD: make this enum ?
 NORMAL = "NORMAL"
 SUSPICIOUS = "SUSPICIOUS"
 DANGEROUS = "DANGEROUS"
 CRITICAL = "CRITICAL"
 
 
-class Attack():
-    def __init__(self, name, severity, inspect):
+class Attack:
+    def __init__(self, name, severity, inspect, logger):
         self.name = name
         self.severity = severity
         self._inspect = inspect
+        self.logger = logger
     
     def inspect(self, pkt):
         if (data := self._inspect(pkt)):
             date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            print(f'{Fore.RED}{date}: {self.severity}: A possible {self.name} was detected!\n{Fore.RESET}{data}')
+
+            print(
+                f"{Fore.RED}{date}: {self.severity}: "
+                f"A possible {self.name} was detected!\n{Fore.RESET}{data}\n"
+            )
+
+            if self.logger:
+                self.logger.info(
+                    f"{date}: {self.severity}: A possible {self.name} was detected!\n{data}\n"
+                )
 
 
-attacks = [
-    Attack("Randomized MAC Address", NORMAL, randomized_mac.inspect),
-    Attack("TCP SYN Scan", SUSPICIOUS, syn_scan.inspect),
-    Attack("Directory Traversal Attack", DANGEROUS, dir_traversal.inspect),
-    Attack("ARP Spoofing Attack", CRITICAL, arp_spoof.inspect),
-    Attack("SYN Flood Attack", CRITICAL, syn_flood.inspect)
-]
+def load_attacks(logger):
+    attacks = [
+        Attack("Randomized MAC Address", NORMAL, randomized_mac.inspect, logger),
+        Attack("TCP SYN Scan", SUSPICIOUS, syn_scan.inspect, logger),
+        Attack("Directory Traversal", DANGEROUS, dir_traversal.inspect, logger),
+        Attack("ARP Spoofing", CRITICAL, arp_spoof.inspect, logger),
+        Attack("SYN Flood", CRITICAL, syn_flood.inspect, logger),
+        Attack("Brute Force SSH", SUSPICIOUS, ssh_bruteforce.inspect, logger)
+    ]
+    
+    return attacks
--- code/main.py
+++ code/main.py
@@ -1,18 +1,38 @@
-from scapy.all import *
+from utils.logger import get_logger
+logger = get_logger()
+
+import attacks
+from scapy.all import sniff
 from os import system
-system('clear')
+import time
 
-from attacks import attacks
-import general
-general.verbose = True
+def clear_screen():
+    try:
+        system("clear")
+        system("cls")
 
+    except:
+        pass
 
-print(f"LanSec - Local Area Network Security\n{'-'*36}\n")
+def main():
 
+    clear_screen()
 
-def snf(pkt):
-    for attack in attacks:
-        attack.inspect(pkt)
-
+    logger.info("Starting LanSec...")
+    logger.info("Initializing Detection Engine...")
     
-sniff(prn=snf, store=0)
+    searched_attacks = attacks.load_attacks(logger)
+    logger.info("Detection Engine Initialized.")
+
+    time.sleep(3)
+    clear_screen()
+    print("LanSec - Local Area Network Security\n" + "-"*36)
+
+    def snf(pkt):
+        for attack in searched_attacks:
+            attack.inspect(pkt)
+
+    sniff(prn=snf, store=0)
+
+if __name__ == "__main__":
+    main()
