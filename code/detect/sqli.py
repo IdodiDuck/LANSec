1d5d046614bd6af116f087d26fcb7c5453b7b481
IdodiDuck <ido.redid@gmail.com>
2025-12-14 18:23:43 +0200

    refactor (detect): extension of SQLi including static files ignorance

--- code/detect/sqli.py
+++ code/detect/sqli.py
@@ -1,36 +1,54 @@
-# from utils.http_normalizer import HTTPNormalizer
+from utils.http_normalizer import HTTPNormalizer
 from scapy.all import *
 import re
 
+STATIC_EXTENSIONS = (
+    ".css", ".js", ".png", ".jpg", ".jpeg", ".gif",
+    ".svg", ".woff", ".woff2", ".ttf", ".ico"
+)
+
+SQLI_PATTERNS = [
+    r"(\%27)|(\')|(\-\-)|(\%23)|(#)",  # SQL meta-characters
+    r"((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))",  # SQL operators
+    r"\b(OR|AND)\b.+\=",  # Logical operators
+    r"UNION(\s+ALL)?\s+SELECT",  # UNION SELECT
+    r"SELECT\s+.*\s+FROM\s+",  # Basic SELECT statement
+    r"INSERT\s+INTO\s+",  # Basic INSERT statement
+    r"UPDATE\s+.*\s+SET\s+",  # Basic UPDATE statement
+    r"DELETE\s+FROM\s+",  # Basic DELETE statement
+    r"DROP\s+TABLE\s+",  # Basic DROP TABLE statement
+]
 
 def is_valid(payload) -> bool:
-    # Simple SQLi patterns
-    sqli_patterns = [
-        r"(\%27)|(\')|(\-\-)|(\%23)|(#)",  # SQL meta-characters
-        r"((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))",  # SQL operators
-        r"\b(OR|AND)\b.+\=",  # Logical operators
-        r"UNION(\s+ALL)?\s+SELECT",  # UNION SELECT
-        r"SELECT\s+.*\s+FROM\s+",  # Basic SELECT statement
-        r"INSERT\s+INTO\s+",  # Basic INSERT statement
-        r"UPDATE\s+.*\s+SET\s+",  # Basic UPDATE statement
-        r"DELETE\s+FROM\s+",  # Basic DELETE statement
-        r"DROP\s+TABLE\s+",  # Basic DROP TABLE statement
-    ]
-
-    for pattern in sqli_patterns:
+    for pattern in SQLI_PATTERNS:
         if re.search(pattern, payload, re.IGNORECASE):
             return False
     return True
 
 
 def inspect(pkt):
-    if pkt.haslayer(Raw):
-        payload = pkt[Raw].load.decode(errors='ignore')
-        if is_valid(payload) == False:
-            return (
-                f"{pkt[IP].src}:{pkt[TCP].sport} → {pkt[IP].dst}:{pkt[TCP].dport}\n"
-                f"payload: {payload.split('\r\n\r\n')[-1]}"
-            )
+    req = HTTPNormalizer.normalize(pkt)
+    if not req:
+        return None
+
+    path = req.path.lower()
+
+    # Ignore static files
+    if path.endswith(STATIC_EXTENSIONS):
+        return None
+
+    # No user input -> No SQLi
+    if "?" not in path and not req.body:
+        return None
+
+    if not is_valid(path) or not is_valid(req.body):
+        return (
+            f"[ALERT] Possible SQL Injection Detected\n"
+            f"src: {req.src} -> dst: {req.dst}\n"
+            f"method: {req.method}\n"
+            f"path: {req.path}\n"
+            f"body: {req.body}\n"
+        )
 
 
 if __name__ == "__main__":
