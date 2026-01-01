9f8b4063a5fd99d7e430395a42d03c46e2c9c0c2
TalAkav <talakav5205@gmail.com>
2026-01-01 20:06:53 +0200

    feat(iptbls): add blocking for ip's using linuxes iptables

---
+++ code/iptbls.py
@@ -0,0 +1,32 @@
+# Module name is negotiable: iptbls (iptables)
+# iptbls.block("<IP_ADDRESS>")
+# prints should be done via logger
+import subprocess
+
+blocked_ips = set()
+def block(ip):
+    subprocess.run(["iptables", "-A", "INPUT", "-s", ip, "-j", "DROP"])
+    blocked_ips.add(ip)
+    print(f"Blocked {ip}")
+
+def unblock(ip):
+    subprocess.run(["iptables", "-D", "INPUT", "-s", ip, "-j", "DROP"])
+    blocked_ips.discard(ip)
+    print(f"Unblocked {ip}")
+
+def clear():
+    subprocess.run(["iptables", "-F"])
+    blocked_ips.clear()
+    print("All blocks cleared")
+
+def status():
+    for ip in blocked_ips:
+        print(ip)
+
+if __name__ == "__main__":
+    try:
+        while True:
+            eval(input(">>> "))
+    except KeyboardInterrupt:
+        clear()
+        print("Exiting")
