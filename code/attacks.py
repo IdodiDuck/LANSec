9960f79a5985afe813483786388e43d7c4c80599
IdodiDuck <ido.redid@gmail.com>
2026-01-06 17:54:09 +0200

    feat: integrated icmp flood module in the attacks module

--- code/attacks.py
+++ code/attacks.py
@@ -8,11 +8,13 @@ from detect import (
     dir_traversal,
     arp_spoof,
     syn_flood,
-    ping_sweep,
+    udp_flood,
+    icmp_flood,
     ssh_bruteforce,
     tcp_connection_scan,
     arp_sweep,
-    udp_flood
+    ping_sweep
+    
 )
 
 NORMAL = "NORMAL"
@@ -53,6 +55,7 @@ def load_attacks(logger):
         Attack("ARP Sweep", SUSPICIOUS, arp_sweep.inspect, logger),
         Attack("SYN Flood", CRITICAL, syn_flood.inspect, logger),
         Attack("UDP Flood", CRITICAL, udp_flood.inspect, logger),
+        Attack("ICMP Flood", CRITICAL, icmp_flood.inspect, logger),
         Attack("Brute Force SSH", SUSPICIOUS, ssh_bruteforce.inspect, logger),
         Attack("TCP Connection Scan", SUSPICIOUS, tcp_connection_scan.inspect, logger)
     ]
