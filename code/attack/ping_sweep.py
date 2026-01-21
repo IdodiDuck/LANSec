7d881e2be91804d9e7f22c16bfd9bd2e21b61369
TalAkav <talakav5205@gmail.com>
2026-01-21 14:35:35 +0200

    fix(attack/ping_sweep): user input

--- code/attack/ping_sweep.py
+++ code/attack/ping_sweep.py
@@ -3,7 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 output = []
 
-def arp_ping(ip):
+def icmp_ping(ip):
     try:
         packet = IP(dst=ip)/ICMP()
         answered = sr(packet, timeout=2, verbose=0)[0]
@@ -14,8 +14,11 @@ def arp_ping(ip):
         print(f"Error pinging {ip}: {e}")
 
 
+print("Starting ICMP ping sweep")
+print("Assuming subnet mask /24")
+baseip = input("Enter base IP (x.x.x): ") or "192.168.1"
 with ThreadPoolExecutor(max_workers=100) as executor:
-    executor.map(lambda ip: arp_ping(f"192.168.1.{ip}"), (i for i in range(1, 256)))
+    executor.map(lambda ip: icmp_ping(f"{baseip}.{ip}"), (i for i in range(1, 256)))
 
 
 # you can clear above for pretty output
